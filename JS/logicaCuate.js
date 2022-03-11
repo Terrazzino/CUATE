ef45d6402123263debc771b4e42da3effe48c8f5
Terrazzino <terrazzinofranco1996@gmail.com>
2022-03-11 13:05:55 -0300

    Agregamos barra de menú

--- JS/logicaCuate.js
+++ JS/logicaCuate.js
@@ -69,3 +69,26 @@ tituloInfo4.addEventListener("click",()=>{
         let isHidden = p4.getAttribute("hidden");
         isHidden ? p4.removeAttribute("hidden") : p4.setAttribute("hidden","true");
 })
+
+const menu = document.querySelector(".bttn-menu");
+const inicio = document.querySelector(".inicio-div");
+const historia = document.querySelector(".historia-div");
+const trabajo = document.querySelector(".trabajos-div");
+const contactanos = document.querySelector(".contactanos-div");
+
+menu.addEventListener("click",()=>{
+    let isHidden = inicio.getAttribute("hidden");
+    isHidden ? inicio.removeAttribute("hidden") : inicio.setAttribute("hidden","true");
+})
+menu.addEventListener("click",()=>{
+    let isHidden = historia.getAttribute("hidden");
+    isHidden ? historia.removeAttribute("hidden") : historia.setAttribute("hidden","true");
+})
+menu.addEventListener("click",()=>{
+    let isHidden = trabajo.getAttribute("hidden");
+    isHidden ? trabajo.removeAttribute("hidden") : trabajo.setAttribute("hidden","true");
+})
+menu.addEventListener("click",()=>{
+    let isHidden = contactanos.getAttribute("hidden");
+    isHidden ? contactanos.removeAttribute("hidden") : contactanos.setAttribute("hidden","true");
+})
